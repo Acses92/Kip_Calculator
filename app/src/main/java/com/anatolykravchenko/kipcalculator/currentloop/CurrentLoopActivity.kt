f3a2ec7a65cda2ec53bcc1715e5f33fc2c0435e2
Acses92 <40656486+Acses92@users.noreply.github.com>
2021-08-19 22:58:22 +0500

    Add editText Listeners and view model

--- app/src/main/java/com/anatolykravchenko/kipcalculator/currentloop/CurrentLoopActivity.kt
+++ app/src/main/java/com/anatolykravchenko/kipcalculator/currentloop/CurrentLoopActivity.kt
@@ -3,7 +3,9 @@ package com.anatolykravchenko.kipcalculator.currentloop
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.anatolykravchenko.kipcalculator.R
 import com.anatolykravchenko.kipcalculator.databinding.CurentLoopActivityBinding
 import com.anatolykravchenko.kipcalculator.databinding.RtdActivityBinding
@@ -15,23 +17,67 @@ class CurrentLoopActivity:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = CurentLoopActivityBinding.inflate(layoutInflater)
         val view = binding.root
+        val currentLoopVM = ViewModelProvider(this).get(CurrentLoopViewModel::class.java)
         setContentView(view)
 
         binding.upermCurrentLevelEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
-            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
+            override fun afterTextChanged(s: Editable?) {
+
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s.isNullOrEmpty() || s.contentEquals(".")) {
+                    currentLoopVM.highLimit = 0.0
+                }else currentLoopVM.highLimit = s.toString().toDouble()
+            }
+        })
 
+        binding.lowCurrentLevelEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s.isNullOrEmpty() || s.contentEquals(".")) {
+                    currentLoopVM.lowLimit = 0.0
+                }else currentLoopVM.lowLimit = s.toString().toDouble()
+            }
+        })
+
+        binding.currentValueEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s.isNullOrEmpty() || s.contentEquals(".")) {
+                    currentLoopVM.value = 0.0
+                }else currentLoopVM.value = s.toString().toDouble()
             }
         })
 
 
+        binding.currentResultButton.setOnClickListener {
+
+        }
+
     }
+    fun inputValueChecker(lowLimit: Double, highLimit: Double, value: Double) {
+        if(lowLimit>highLimit) {
+            Toast.makeText(applicationContext, "Нижнее значение не может быть больше верхнего",
+                Toast.LENGTH_SHORT).show()
+        }
+
+        if(value>highLimit || value <lowLimit) {
+            Toast.makeText(applicationContext, "Вы ввели значение вне пределов",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
+
--- app/src/main/java/com/anatolykravchenko/kipcalculator/currentloop/CurrentLoopViewModel.kt
+++ app/src/main/java/com/anatolykravchenko/kipcalculator/currentloop/CurrentLoopViewModel.kt
@@ -1,8 +1,13 @@
 package com.anatolykravchenko.kipcalculator.currentloop
 
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 
 class CurrentLoopViewModel: ViewModel() {
+
+    var highLimit: Double = 0.0
+    var lowLimit: Double = 0.0
+    var value: Double = 0.0
 
     fun getCurrent(lowLimit: Double, highLimit: Double, value: Double): Double {
         return lowLimit
@@ -10,6 +15,7 @@ class CurrentLoopViewModel: ViewModel() {
 
     fun getValue(lowLimit: Double, highLimit: Double, current: Double): Double {
 
-        return (((current-4.0)*(highLimit-lowLimit))/16)+lowLimit
+        return (((current - 4.0) * (highLimit - lowLimit)) / 16) + lowLimit
     }
+
 }
