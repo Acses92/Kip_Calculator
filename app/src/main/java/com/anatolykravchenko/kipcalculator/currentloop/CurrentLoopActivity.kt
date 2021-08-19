e8fbb91a24727c097d328eadbca63c0808be516a
Acses92 <40656486+Acses92@users.noreply.github.com>
2021-08-19 21:39:03 +0500

    Fix problem with "." crush at rtd activity.

--- app/src/main/java/com/anatolykravchenko/kipcalculator/currentloop/CurrentLoopActivity.kt
+++ app/src/main/java/com/anatolykravchenko/kipcalculator/currentloop/CurrentLoopActivity.kt
@@ -1,6 +1,8 @@
 package com.anatolykravchenko.kipcalculator.currentloop
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import com.anatolykravchenko.kipcalculator.R
 import com.anatolykravchenko.kipcalculator.databinding.CurentLoopActivityBinding
@@ -14,5 +16,22 @@ class CurrentLoopActivity:AppCompatActivity() {
         binding = CurentLoopActivityBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.upermCurrentLevelEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+
+            }
+        })
+
+
     }
 }
--- app/src/main/java/com/anatolykravchenko/kipcalculator/rtd/RTDActivity.kt
+++ app/src/main/java/com/anatolykravchenko/kipcalculator/rtd/RTDActivity.kt
@@ -96,7 +96,7 @@ class RTDActivity: AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {}
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if(s.contentEquals(""))
+                if(s.contentEquals(".") || s.isNullOrEmpty())
                 {
                     RTDViewModel.inputValue = 0.0
                     binding.rtdResultButton.isEnabled = false
